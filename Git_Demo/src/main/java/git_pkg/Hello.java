575bfe2e301a919cac80fac4d81b3e5215acf8bf
NMShaik <sonusallu786@gmail.com>
2023-07-08 17:51:08 +0530

    remove_this_code

--- Git_Demo/src/main/java/git_pkg/Hello.java
+++ Git_Demo/src/main/java/git_pkg/Hello.java
@@ -3,10 +3,6 @@ package git_pkg;
 public class Hello
 {
 public static void main(String[] args) {
-	
-	int a=10;
-	int b=20;
-	int c=a+b;
-	System.out.println(c);
+	System.out.println("Hello World");
 }
 }
