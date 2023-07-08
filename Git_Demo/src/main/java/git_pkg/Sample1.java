587056a99ea0de5c1b546141e11cb8812d029b58
NMShaik <sonusallu786@gmail.com>
2023-07-08 18:57:12 +0530

    data

--- Git_Demo/src/main/java/git_pkg/Sample1.java
+++ Git_Demo/src/main/java/git_pkg/Sample1.java
@@ -1,15 +1,14 @@
 package git_pkg;
 
 
-public class Sampl1 
+public class Sample1 
 {
-<<<<<<< Updated upstream
+
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("hfakyf");
 	}
-=======
->>>>>>> Stashed changes
+
 
 
 }
