4a7e97eaaad2a00b7efff5a2b50c9fbb3fc95ab4
NMShaik <sonusallu786@gmail.com>
2023-07-08 19:01:02 +0530

    Update the same data

--- Git_Demo/src/main/java/git_pkg/Sampl1.java
+++ Git_Demo/src/main/java/git_pkg/Sampl1.java
@@ -2,12 +2,19 @@ package git_pkg;
 
 public class Sampl1 
 {
+<<<<<<< Updated upstream:Git_Demo/src/main/java/git_pkg/Sampl1.java
 <<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes:Git_Demo/src/main/java/git_pkg/Sample1.java
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("hfakyf");
+		System.out.println("Hello India");
+		
 	}
+<<<<<<< Updated upstream:Git_Demo/src/main/java/git_pkg/Sampl1.java
 =======
 >>>>>>> Stashed changes
 
+=======
+>>>>>>> Stashed changes:Git_Demo/src/main/java/git_pkg/Sample1.java
 }
