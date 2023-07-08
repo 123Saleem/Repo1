54c7f32a2472672943548bb7bb95d6812b3cc4c1
NMShaik <sonusallu786@gmail.com>
2023-07-08 18:55:19 +0530

    data

--- Git_Demo/src/main/java/git_pkg/Sampl1.java
+++ Git_Demo/src/main/java/git_pkg/Sampl1.java
@@ -2,9 +2,12 @@ package git_pkg;
 
 public class Sampl1 
 {
+<<<<<<< Updated upstream
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("hfakyf");
 	}
+=======
+>>>>>>> Stashed changes
 
 }
