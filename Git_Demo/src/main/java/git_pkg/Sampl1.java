3f826d11be21e670c3a953895539759e6182e2e8
NMShaik <sonusallu786@gmail.com>
2023-07-08 18:49:56 +0530

    same_add

--- Git_Demo/src/main/java/git_pkg/Sampl1.java
+++ Git_Demo/src/main/java/git_pkg/Sampl1.java
@@ -1,19 +1,11 @@
 package git_pkg;
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-public class Sampl1 {
-=======
-public class Sampl1 
-{
-	
->>>>>>> Stashed changes
-=======
+
 public class Sampl1 
 {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 	}
->>>>>>> c33c97f0ad5919109616de87af9aede245e86db0
+
 
 }
