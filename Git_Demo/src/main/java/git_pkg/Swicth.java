bb85f3dfeb6163ad170627c196f53aac6d371877
NMShaik <sonusallu786@gmail.com>
2023-07-10 11:15:15 +0530

    create_new_code

---
+++ Git_Demo/src/main/java/git_pkg/Swicth.java
@@ -0,0 +1,18 @@
+package git_pkg;
+
+public class Swicth 
+{
+	public static void main(String[] args) {
+		char ch='a';
+		switch(ch) {
+		case 'A':
+			System.out.println("A is Character");
+		case 'Q':
+			System.out.println("Q is Character");
+		case 'a':
+			System.out.println("a is Character");
+			default:
+				System.out.println("This all character are A or Q or a");
+		}
+	}
+}
