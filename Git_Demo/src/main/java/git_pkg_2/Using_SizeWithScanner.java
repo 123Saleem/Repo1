81f6eae9111f8686ec36441f047d7aa36d865509
NMShaik <sonusallu786@gmail.com>
2023-07-10 11:34:15 +0530

    Create_SizePro

---
+++ Git_Demo/src/main/java/git_pkg_2/Using_SizeWithScanner.java
@@ -0,0 +1,33 @@
+package git_pkg_2;
+
+import java.util.Scanner;
+
+public class Using_SizeWithScanner 
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Size Number");
+		int num;
+		String size;
+		num=sc.nextInt();
+		switch(num) {
+		case 10:
+			size="Small";
+			System.out.println("Size is "+size);
+			break;
+		case 20:
+			size="Medium";
+			System.out.println("Size is "+size);
+			break;
+		case 30:
+			size= "Large";
+			System.out.println("Size is "+size);
+			break;
+			default:
+				System.out.println("Size is Not there");
+				
+		}
+		sc.close();
+	}
+
+}
