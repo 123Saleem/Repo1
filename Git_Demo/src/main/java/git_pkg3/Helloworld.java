520630d1388c044333bcbeea4c2ba94ff55f2af9
NMShaik <sonusallu786@gmail.com>
2023-07-10 18:03:03 +0530

    Update Helloworld.java

--- Git_Demo/src/main/java/git_pkg3/Helloworld.java
+++ Git_Demo/src/main/java/git_pkg3/Helloworld.java
@@ -5,6 +5,8 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloo world");
+		System.out.println("iam good ");
+		System.out.println("wr r u");
 
 	}
 
