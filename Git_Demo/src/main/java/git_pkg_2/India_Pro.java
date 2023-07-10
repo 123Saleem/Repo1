f4bf43c0f0602dec979a4b2603d8fbc805f01559
NMShaik <sonusallu786@gmail.com>
2023-07-10 16:10:58 +0530

    update

--- Git_Demo/src/main/java/git_pkg_2/India_Pro.java
+++ Git_Demo/src/main/java/git_pkg_2/India_Pro.java
@@ -6,5 +6,8 @@ public static void main(String[] args) {
 	System.out.println("Hello India");
 	System.out.println("Hello World");
 	System.out.println("Hi Mama");
+	System.out.println("Hello India");
+	System.out.println("Hello World");
+	System.out.println("Hi Mama");
 }
 }
