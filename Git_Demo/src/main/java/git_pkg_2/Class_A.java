ffb975b955aceadc36b2dd694c470c47b1255193
NMShaik <sonusallu786@gmail.com>
2023-07-10 16:57:43 +0530

    sameAdd

--- Git_Demo/src/main/java/git_pkg_2/Class_A.java
+++ Git_Demo/src/main/java/git_pkg_2/Class_A.java
@@ -2,7 +2,8 @@ package git_pkg_2;
 
 public class Class_A 
 {
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
 	System.out.println("Hi");
 	System.out.println("Hello");
 	System.out.println("");
