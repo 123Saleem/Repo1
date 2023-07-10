d65a23825616952ce56f79932e9548a814cf1e6f
NMShaik <sonusallu786@gmail.com>
2023-07-10 11:37:12 +0530

    Create_String

---
+++ Git_Demo/src/main/java/git_pkg_2/Using_String.java
@@ -0,0 +1,22 @@
+package git_pkg_2;
+
+public class Using_String 
+{
+	public static void main(String[] args) {
+		String course="Java";
+		
+		switch(course) {
+		case "Python":
+			System.out.println("Learn Python Language");
+			break;
+		case "c++":
+			System.out.println("Learn C++ Language");
+			break;
+		case "Java":
+			System.out.println("Learn Java Language");
+			break;
+		default:
+			System.out.println("don't have languages");
+		}
+	}
+}
